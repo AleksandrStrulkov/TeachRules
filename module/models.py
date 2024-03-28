0bb0d60fc49c74b99dafe45bcd351f342989048d
AleksandrStrulkov <astrulkov@yandex.ru>
2024-03-28 09:27:26 +0300

    Change model module

--- module/models.py
+++ module/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db import connection
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 NULLABLE = {'blank': True, 'null': True}
@@ -30,7 +32,7 @@ class Subject(models.Model):
 
 class Block(models.Model):
     creator = models.ForeignKey(get_user_model(), related_name='courses_created', on_delete=models.CASCADE,
-                                verbose_name='Создатель')
+                                verbose_name='Владелец')
     subject = models.ForeignKey(Subject, related_name='blocks', on_delete=models.CASCADE, verbose_name='Тема')
     title = models.CharField(max_length=200, verbose_name='Название блока')
     slug = models.SlugField(max_length=200, unique=True, verbose_name='slug', **NULLABLE)
@@ -72,3 +74,49 @@ class Module(models.Model):
                 f'ALTER SEQUENCE '
                 f'catalog_statusproduct_id_seq RESTART WITH 1;'
                 )
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,
+                               related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in':(
+                                     'text',
+                                     'video',
+                                     'image',
+                                     'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    creator = models.ForeignKey(get_user_model(),
+                              related_name='%(class)s_related',
+                              on_delete=models.CASCADE,
+                              verbose_name='Владелец')
+    title = models.CharField(max_length=250, verbose_name='Название')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField(verbose_name='Текст')
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files', verbose_name='PDF файл', **NULLABLE)
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images', verbose_name='Изображение', **NULLABLE)
+
+
+class Video(ItemBase):
+    url = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
